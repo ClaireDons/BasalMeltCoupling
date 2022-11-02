c6f08692e1bf9f4d765b56353addc695f9a8a009
Claire Donnelly <nlcd@aa6-100.bullx>
2022-11-02 10:13:30 +0000

    Initial commit

---
+++ bisicles_coupledbm.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -l
+#SBATCH -J testrun
+#SBATCH -q np
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=128
+#SBATCH -t 12:00:00
+
+module load prgenv/gnu
+module load python3/3.8.8-01
+module load gcc/11.2.0
+module load openmpi/4.1.1.1
+module load hdf5-parallel/1.10.6
+module load netcdf4-parallel/4.7.4
+
+# Define driver 
+export DRIVER=/perm/nlcd/bisicles/BISICLES/code/exec2D/driver2d.Linux.64.mpiCC.mpif90.DEBUG.OPT.MPI.PETSC.ex
+
+# Define input files
+export INFILEBASE="inputs.setup"
+export INFILE=inputs.setup.$SLURM_JOBID
+echo "INFILE = $INFILE"
+cp $INFILEBASE $INFILE
+
+n=coupled_bm
+bm=sb_ssp585_lev_2000.2d.hdf5
+
+count=$(find chk.* -maxdepth 1 -type f|wc -l)
+echo $count
+let count=count+1 # Increase by one, for the next file number
+echo $count
+
+sed -i s/@NAME/$n/ $INFILE
+sed -i s/@BM_DATA/$bm/ $INFILE
+sed -i s/@TIME/$count/ $INFILE
+
+export PYTHONPATH=`pwd`
+export LD_LIBRARY_PATH=$HDF5_PARALLEL_DIR/lib:$PYTHON3_DIR/lib:$LD_LIBRARY_PATH
+export CH_OUTPUT_INTERVAL=0
+
+ # work out what the latest checkpoint file is (if it exists)
+if test -n "$(find -type f -name "chk.$n.??????.2d.hdf5" -print -quit)"
+    then
+    LCHK=`ls -th chk.$n.??????.2d.hdf5 | head -n 1`
+    echo "" >> $INFILE #ensure line break
+    echo "amr.restart_file=$LCHK" >> $INFILE
+    echo "amr.restart_set_time=false" >> $INFILE
+    echo "" >> $INFILE #ensure line break
+fi
+
+srun $DRIVER $INFILE
---
+++ bm_coupling.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+
+### 1. Check for last plot file, if one exists, take most recent one and flatten to NetCDF, 
+###    If no plot file then use time=0 values
+
+n=coupled_bm
+NC_PLOT=plot.$n.nc
+FLATTEN="/perm/nlcd/bisicles/BISICLES/code/filetools/flatten2d.Linux.64.mpiCC.gfortran.DEBUG.MPI.ex"
+SH_BISICLES="bisicles_coupledbm.sh"
+
+
+if test -n "$(find -type f -name "plot.$n.??????.2d.hdf5" -print -quit)"
+    then
+    echo "Found plot"
+    LPLOT=`ls -th plot.$n.??????.2d.hdf5 | head -n 1`
+    # Flatten the plot
+    $FLATTEN $LPLOT $NC_PLOT 0 0 0
+    # Pass on new fields to input file
+    echo "$NC_PLOT"
+fi
+
+### 2. Run basal melt python script
+
+### 3. Define new basal melt values in input file
+
+### 4. Run BISICLES and stop once 1 plot file has been created
+sbatch bisicles_coupledbm.sh
+
+# 
+#        sed -i s/@GEO_FILE/$LPLOT/ $SH_BISICLES
+#        sed -i s/@C_FILE/$LPLOT/ $SH_BISICLES
+#        sed -i s/@MU_FILE/$LPLOT/ $SH_BISICLES
+#

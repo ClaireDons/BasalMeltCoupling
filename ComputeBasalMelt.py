92641dbd67d8761de89b03d65af25eed8b71d31c
“Claire <claire.donnelly@knmi.nl>
2022-11-03 14:53:16 +0000

    rearranged things and added comments

--- ComputeBasalMelt.py
+++ ComputeBasalMelt.py
@@ -6,26 +6,6 @@ import pandas as pd
 
 import basal_melt_calc as calc
 
-# Define paths
-gen_path = "/nobackup/users/donnelly/"
-# in
-mask_path = gen_path + "levermann-masks/"
-# out
-nc_out = gen_path + 'BasalMeltCoupling/'
-
-nc_files = glob(os.path.join(mask_path, "*.2d.nc")) # sector masks
-driver = '/usr/people/donnelly/bisicles/BISICLES/code/filetools/nctoamr2d.Linux.64.g++.gfortran.DEBUG.ex' # nc to amr hdf5 tool 
-
-# Open ncfiles
-for file in nc_files:
-    key = os.path.splitext(os.path.basename(file))[0][15:-3]
-    name = 'smask' + str(key)
-    dat = xr.open_dataset(file)
-    globals()['smask' + str(key)] = np.array(dat['smask'])
-
-x = np.array(dat['x'])
-y = np.array(dat['y'])
-
 # Define parameters
 path = "/net/pc200037/nobackup/users/linden/cmip6data/CMIP6/CMIP/EC-Earth-Consortium/EC-Earth3/historical/r1i1p1f1/" 
 area_file = path + "Ofx/areacello/gn/areacello_Ofx_EC-Earth3_historical_r1i1p1f1_gn.nc"
@@ -35,8 +15,29 @@ gamma = 0.05
 Tf = -1.6
 baseline = 1
 
+# Define paths
+gen_path = "/nobackup/users/donnelly/"
+mask_path = gen_path + "levermann-masks/"
+nc_out = gen_path + 'BasalMeltCoupling/'
+
+# Load leverman masks (Maybe in future should just be replaces with coordinates)
+nc_files = glob(os.path.join(mask_path, "*.2d.nc")) # sector masks
+driver = '/usr/people/donnelly/bisicles/BISICLES/code/filetools/nctoamr2d.Linux.64.g++.gfortran.DEBUG.ex' # nc to amr hdf5 tool 
+
+# Open ncfiles and create masks for BISICLES
+for file in nc_files:
+    key = os.path.splitext(os.path.basename(file))[0][15:-3]
+    name = 'smask' + str(key)
+    dat = xr.open_dataset(file)
+    globals()['smask' + str(key)] = np.array(dat['smask'])
+
+x = np.array(dat['x'])
+y = np.array(dat['y'])
+
+# Calculate volume weighted mean
 df = calc.weighted_mean_df(area_file, thetao_file, sectors)
 
+# Calculate basal melt
 df2 =  pd.DataFrame()
 for column in df:
     thetao = df[column].values

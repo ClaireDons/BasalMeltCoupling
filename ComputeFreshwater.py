7dde9f2b969320b979f5a3bebf60531e2ae47562
“Claire <claire.donnelly@knmi.nl>
2022-11-28 16:43:53 +0000

    Deleted some unnecessary lines

--- ComputeFreshwater.py
+++ ComputeFreshwater.py
@@ -14,12 +14,6 @@ from AMRtools import flatten as flat
 # Define parameters
 path = "/nobackup/users/donnelly/BasalMeltCoupling/"
 
-# Define paths
-mask_path = "/nobackup/users/donnelly/levermann-masks"
-nc_out = path
-
-driver = '/perm/nlcd/bisicles/BISICLES/code/filetools/nctoamr2d.Linux.64.mpiCC.gfortran.DEBUG.MPI.ex' # nc to amr hdf5 tool 
-
 filetoolsPath = r'/usr/people/donnelly/bisicles/BISICLES/code/filetools/'
 filetoolFlatten = 'flatten2d.Linux.64.g++.gfortran.DEBUG.ex'
 

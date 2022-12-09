151a4e07d4c9aba29b4f100fa779a636c34ba6af
Claire Donnelly <nlcd@ad6-102.bullx>
2022-12-09 19:59:30 +0000

    Got things to work on atos

--- ComputeFreshwater.py
+++ ComputeFreshwater.py
@@ -3,17 +3,21 @@ from glob import iglob
 
 
 # Define paths
-path =  "/nobackup/users/donnelly/Antarctica/ssp585/shelfbasedepth/"
-mask_path = "/nobackup/users/donnelly/levermann-masks/"
-nc_out = "/nobackup/users/donnelly/BasalMeltCoupling/"
+path =  "/ec/res4/scratch/nlcd/CMIP6/bm_coupling/"
+mask_path = path + "levermann_masks/"
+nc_out = path 
+plot_path = path + "plots/"
 
 # Define parameters
-filetoolsPath = r'/usr/people/donnelly/bisicles/BISICLES/code/filetools/'
-filetoolFlatten = 'flatten2d.Linux.64.g++.gfortran.DEBUG.ex'
+filetoolsPath = r'/perm/nlcd/bisicles/BISICLES/code/filetools/'
+filetoolFlatten = 'flatten2d.Linux.64.mpiCC.gfortran.DEBUG.MPI.ex'
 flatten = filetoolsPath + filetoolFlatten
 
-LatestFile = sorted(iglob(path + "*.2d.hdf5"), reverse = True)[0]
-PenultimateFile = sorted(iglob(path + "*.2d.hdf5"), reverse = True)[1]
+PenultimateFile = sorted(iglob(plot_path + "*.2d.hdf5"), reverse = True)[1]
+LatestFile = sorted(iglob(plot_path + "*.2d.hdf5"), reverse = True)[0]
+print(PenultimateFile)
+print(LatestFile)
+
 
 fw = FW.Freshwater(flatten, PenultimateFile, LatestFile)
 discharge, basal = fw.RegionalContribution(mask_path,nc_out,flatten)

864ce8865ae3348c06c40bcb0daebd5647ec0f96
Claire Donnelly <nlcd@ad6-100.bullx>
2023-02-03 14:52:51 +0000

    Added waiting for model run to finish

--- BasalMeltCoupling.sh
+++ BasalMeltCoupling.sh
@@ -10,3 +10,16 @@ for k in {2..3};
         jid=$(sbatch --dependency=afterok:${jid} ECE_BISI_basal${k}.slurm | cut -d ' ' -f4)
         echo $jid
     done
+
+## Wait for last job to finish before exiting script
+sentence=$(squeue -j $jid | tr -s ' ' | cut -d ' ' -f6)
+echo $sentence
+
+until [[ "$sentence" =~ .*"COMPLETI".* ]]
+do
+    sentence=$(squeue -j $jid | tr -s ' ' | cut -d ' ' -f6)
+    echo $sentence
+    sleep 10
+done
+
+echo "done"

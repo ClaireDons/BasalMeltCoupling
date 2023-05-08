eac83429d95d9c9795c2b466c929ed3b8ac864bb
Claire Donnelly <nlcd@ac6-102.bullx>
2023-05-08 14:21:08 +0000

    Fixed some paths and added to requirements

--- BasalMeltCoupling.sh
+++ BasalMeltCoupling.sh
@@ -2,7 +2,7 @@
 
 # 1. Load relevant modules
 module load prgenv/gnu
-module load python3/3.8.8-01
+#module load python3/3.8.8-01
 module load gcc/11.2.0
 module load openmpi/4.1.1.1
 module load hdf5-parallel/1.10.6
@@ -11,19 +11,29 @@ module load netcdf4-parallel/4.7.4
 # 2. Export correct paths to python
 export PYTHONPATH=`pwd`
 export LD_LIBRARY_PATH=$HDF5_PARALLEL_DIR/lib:$PYTHON3_DIR/lib:$LD_LIBRARY_PATH
-#source venv/bin/activate
 
 # 3. Define names and paths needed
 ### experiment name and variables
-exp_name=COUPLING_TEST
 gamma=0.05
 bm_name=basal_melt
 bm_file=$bm_name.2d.hdf5
 
+# Variables passed on from EC-Earth
+leg_number=$1
+exp_name=$2
+start_dir=$3
+run_dir=$4 
+
+echo "leg number: $leg_number, exp_name: $exp_name, start_dir: $start_dir, run_dir: $run_dir"
+
 ### paths
 scratch_path=/scratch/nlcd
+nemo_output=$run_dir/output/nemo/$leg_number/
+echo "$nemo_output"
 BISI_INPUT=$scratch_path/bisicles_setup 
-outpath=$scratch_path/$exp_name
+outpath=$run_dir/output/bisicles
+echo "$outpath"
+#outpath=$scratch_path/$exp_name
 plots=$outpath/plots/hdf5/
 
 # BISICLES paths + driver and tools
@@ -32,12 +42,16 @@ FLATTEN="$BISICLES_HOME/code/filetools/flatten2d.Linux.64.mpiCC.mpif90.DEBUG.OPT
 NC2AMR="$BISICLES_HOME/code/filetools/nctoamr2d.Linux.64.mpiCC.mpif90.DEBUG.OPT.MPI.PETSC.ex"
 DRIVER="$BISICLES_HOME/code/exec2D/driver2d.Linux.64.mpiCC.mpif90.DEBUG.OPT.MPI.PETSC.ex"
 
-### 4. Run basal melt python script    
-python3 compute_basalmelt.py $exp_name $gamma $bm_name $scratch_path $NC2AMR ${num} || exit
+module load python3
+source $start_dir/BasalMeltCoupling/venv/bin/activate
+pip3 install -r $start_dir/BasalMeltCoupling/requirements.txt
 
-### 5. Define new basal melt values in input files
-COUPLED_TEMPLATE="BISICLES_submission_template.slurm"
-export COUPLED=BISICLES_submission.slurm
+### 4. Run basal melt python script    
+python3 $start_dir/BasalMeltCoupling/compute_basalmelt.py $exp_name $gamma $bm_name $start_dir $outpath $nemo_output $NC2AMR ${num} || exit
+
+### 5. Define new basal melt values in input files /// Currently if this fails then the script still keeps running, build test in. 
+COUPLED_TEMPLATE="$start_dir/BasalMeltCoupling/BISICLES_submission_template.slurm"
+export COUPLED=$start_dir/BasalMeltCoupling/BISICLES_submission.slurm
 cp $COUPLED_TEMPLATE $COUPLED
 
 sed -i s+@melt+$bm_file+ $COUPLED
@@ -47,18 +61,18 @@ sed -i s+@driver+$DRIVER+ $COUPLED
 sed -i s+@bisi+$BISI_INPUT+ $COUPLED
 
 # 6. Get job id and wait for BISICLES to finish running
-jid=$(sbatch BISICLES_submission.slurm| cut -d ' ' -f4)
+jid=$(sbatch $start_dir/BasalMeltCoupling/BISICLES_submission.slurm| cut -d ' ' -f4)
 echo $jid
 echo "waiting for BISICLES..."
-until test -f "$jid.txt"
+until test -f "$start_dir/BasalMeltCoupling/$jid.txt"
 do
     sleep 60
 done
-rm $jid.txt
+rm $start_dir/BasalMeltCoupling/$jid.txt
 
-if test -f err.0
+if test -f $start_dir/BasalMeltCoupling/err.0
     then
-    if grep -q "srun: error:" "err.0"
+    if grep -q "srun: error:" "$start_dir/BasalMeltCoupling/err.0"
     then
         echo "BISICLES encountered an error, exiting."
         exit
@@ -71,7 +85,7 @@ echo "...BISICLES done!"
 if test -n "$(find $plots -type f -name "plot.$exp_name.??????.2d.hdf5" -print -quit)"
     then
     echo "Found plot, calculating freshwater"
-    python3 compute_freshwater.py $exp_name $scratch_path $FLATTEN ${num} || exit
+    python3 $start_dir/BasalMeltCoupling/compute_freshwater.py $exp_name $scratch_path $FLATTEN ${num} || exit
     else
     echo "Something went wrong, no plot"
     exit 
--- compute_basalmelt.py
+++ compute_basalmelt.py
@@ -20,26 +20,26 @@ GAMMA = float(sys.argv[2])
 NAME = str(sys.argv[3])
 
 # Define Paths
-PATH = os.path.dirname(os.path.realpath(__file__))
+PATH = str(sys.argv[4]) + "/BasalMeltCoupling"
 MASK_PATH = PATH + "/inputs/levermann_masks/"
 
-SCRATCH = str(sys.argv[4]) + "/"
-EXP_OUT = SCRATCH + EXP_NAME + "/"
-NC_OUT = EXP_OUT + "plots/nc/"
-PLOT_PATH = EXP_OUT + "plots/hdf5/"
-CSV_OUT = EXP_OUT + "csv/"
-CHK_OUT = EXP_OUT + "checkpoints/"
+OUTPATH = str(sys.argv[5]) + "/"
+NC_OUT = OUTPATH + "/plots/nc/"
+PLOT_PATH = OUTPATH + "/plots/hdf5/"
+CSV_OUT = OUTPATH + "/csv/"
+CHK_OUT = OUTPATH + "/checkpoints/"
 
 AREA_FILE = (
     PATH + "/inputs/ec-earth_data/areacello_Ofx_EC-Earth3_historical_r1i1p1f1_gn.nc"
 )
 
-NEMO_PATH = SCRATCH + "r9469-cmip6-bisi-knmi/TEST/output/nemo/001/"
+NEMO_PATH = str(sys.argv[6])
+print(NEMO_PATH)
 THETAO_FILE = sorted(iglob(NEMO_PATH + "*_grid_T.nc"))[0]
+print(THETAO_FILE)
 
 # Load leverman masks (Maybe in future should just be replaces with coordinates)
-DRIVER = str(sys.argv[5])
-
+DRIVER = str(sys.argv[7])
 
 def new_path(path_name):
     if not os.path.exists(path_name):
@@ -54,6 +54,6 @@ if __name__ == "__main__":
     OUTPUT_CSV = new_path(CSV_OUT)
     OUTPUT_CHK = new_path(CHK_OUT)
     OCEAN_TEMP = BM.BasalMelt(THETAO_FILE, AREA_FILE, GAMMA)
-    OCEAN_TEMP.map_basalmelt(MASK_PATH, EXP_OUT, DRIVER, NAME)
+    OCEAN_TEMP.map_basalmelt(MASK_PATH, OUTPATH, DRIVER, NAME)
 
     print("Basal Melt Calculated")
